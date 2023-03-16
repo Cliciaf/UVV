75550330e5d4f2ff3c98a9fe928f632316b2e0b2
Clicia <89753006+Cliciaf@users.noreply.github.com>
2023-03-16 13:40:06 +0000

    busca binaria recursiva

--- Estruturas de Dados/recursividade/buscabinaria.c
+++ Estruturas de Dados/recursividade/buscabinaria.c
@@ -1,5 +1,34 @@
 #include <stdio.h>
+
+ int buscaBinariaFor(int*v, int min, int max, int elem)
+ {
+
+ }
+ 
+int buscaBinariaRecur (int *v, int min, int max, int elem);
+
+int buscaBinariaRecur (int *v, int min, int max, int elem) 
+ {
+    int meio = (min+max)/2;
+    printf("%d", meio);
+    if(elem > v[meio]){
+        return buscaBinariaRecur(v, meio, max, elem);
+
+    }else if(elem < v[meio]){
+        return buscaBinariaRecur(v, min, meio, elem);
+
+    }else if(elem == v[meio]){ 
+        return v[meio];
+
+    }else{
+        return -1;
+    }
+ }
+ 
 int main()
 {
+   int vetor[10] = {1,2,3,4,5,6,7,8,9,10};
+    
+    printf("%d", buscaBinariaRecur(vetor, 0, 9, 10));
     return 0;
 }
