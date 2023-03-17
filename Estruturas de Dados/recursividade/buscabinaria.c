5c0a2055f87f03a216fe87c80b88f537508e1c93
Clicia <89753006+Cliciaf@users.noreply.github.com>
2023-03-17 19:09:47 +0000

    conclusão

--- Estruturas de Dados/recursividade/buscabinaria.c
+++ Estruturas de Dados/recursividade/buscabinaria.c
@@ -1,20 +1,22 @@
 #include <stdio.h>
-int buscaBinariaFor(int*v, int elem);
-int buscaBinariaFor(int*v, int elem)
-{
-    int min = 0;
-    int max = sizeof(v);
-    int meio = (min+max)/2;
-    if(max < min){
-        return -1
-    }else if(v[meio] < elem){
-        for(int i = meio; i < max; i++){
-            if(v[i] < elem){
-                return i;
-            }
-        }
 
-    }
+int buscaBinFor(int *vet, int buscado,  int min, int max){
+    
+    for (; min <= max; ) { //loop para buscar o valor
+
+        int meio = (min + max) / 2; 
+
+        if (vet[meio] == buscado) {
+            return meio; //retornando indice do vetor em que o valor se encontra
+        
+        } else if (vet[meio] > buscado) {
+            max = meio - 1; //calculo para buscar o elemento solicitado entre numeros que são maiores que o meio até o max
+        
+        } else if(vet[meio] < buscado){
+            min = meio + 1; //calculo para buscar o elemento solicitado entre o minimo e os numeros que são menores que o meio 
+        }else{
+            return -1; //caso não encontre o elemento
+        }
     }
 }
  
@@ -23,20 +25,20 @@ int buscaBinariaRecur (int *v, int min, int max, int elem);
 int buscaBinariaRecur (int *v, int min, int max, int elem) 
  {
     int meio = (min+max)/2;
-    //printf("%d", meio);
+
     if(elem > v[meio]){
-        min = meio + 1;
+        min = meio + 1; //calculo para buscar o elemento solicitado entre numeros que são maiores que o meio até o max
         return buscaBinariaRecur(v, min, max, elem);
 
     }else if(elem < v[meio]){
-        max = meio - 1;
+        max = meio - 1;//calculo para buscar o elemento solicitado entre o minimo e os numeros que são menores que o meio 
         return buscaBinariaRecur(v, min, max, elem);
 
     }else if(elem == v[meio]){ 
-        return v[meio];
+        return meio; //retornando indice do vetor em que o valor se encontra
 
     }else{
-        return -1;
+        return -1; //caso não encontre o elemento
     }
  }
 
@@ -44,6 +46,8 @@ int main()
 {
    int vetor[10] = {1,2,3,4,5,6,7,8,9,10};
     
-    printf("%d", buscaBinariaRecur(vetor, 0, 9, 10));
+    printf("Busca Binaria com Recursividade: o numero buscado está no indice: %d\n", buscaBinariaRecur(vetor, 0, 9, 8));
+    
+    printf("Busca binaria com For: o numero buscado está no indice: %d", buscaBinFor(vetor, 5, 0, 9));
     return 0;
 }
