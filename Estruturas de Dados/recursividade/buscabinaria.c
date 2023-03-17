148d8b0696aed8a865a32554a77f3e7b23a658f2
Clicia <89753006+Cliciaf@users.noreply.github.com>
2023-03-17 18:53:32 +0000

    busca binaria

--- Estruturas de Dados/recursividade/buscabinaria.c
+++ Estruturas de Dados/recursividade/buscabinaria.c
@@ -1,21 +1,36 @@
 #include <stdio.h>
+int buscaBinariaFor(int*v, int elem);
+int buscaBinariaFor(int*v, int elem)
+{
+    int min = 0;
+    int max = sizeof(v);
+    int meio = (min+max)/2;
+    if(max < min){
+        return -1
+    }else if(v[meio] < elem){
+        for(int i = meio; i < max; i++){
+            if(v[i] < elem){
+                return i;
+            }
+        }
 
- int buscaBinariaFor(int*v, int min, int max, int elem)
- {
-
- }
+    }
+    }
+}
  
 int buscaBinariaRecur (int *v, int min, int max, int elem);
 
 int buscaBinariaRecur (int *v, int min, int max, int elem) 
  {
     int meio = (min+max)/2;
-    printf("%d", meio);
+    //printf("%d", meio);
     if(elem > v[meio]){
-        return buscaBinariaRecur(v, meio, max, elem);
+        min = meio + 1;
+        return buscaBinariaRecur(v, min, max, elem);
 
     }else if(elem < v[meio]){
-        return buscaBinariaRecur(v, min, meio, elem);
+        max = meio - 1;
+        return buscaBinariaRecur(v, min, max, elem);
 
     }else if(elem == v[meio]){ 
         return v[meio];
@@ -24,7 +39,7 @@ int buscaBinariaRecur (int *v, int min, int max, int elem)
         return -1;
     }
  }
- 
+
 int main()
 {
    int vetor[10] = {1,2,3,4,5,6,7,8,9,10};
